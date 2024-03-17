fc6c29883051accc47fd0a9f45a67a856d839980
Edris ILYAS <idrsilyas@gmail.com>
2024-03-17 14:15:36 +0300

    final

--- CryptoViper/Entity.swift
+++ CryptoViper/Entity.swift
@@ -14,3 +14,4 @@ struct Crypto : Decodable {
     let currency : String
     let price : String
 }
+ 
--- CryptoViper/Presenter.swift
+++ CryptoViper/Presenter.swift
@@ -9,3 +9,43 @@ import Foundation
 
 //Class, protocol
 // talksto interactor,router,view
+
+enum NetworkError : Error {
+    case NetworkFailed
+    case ParsingFailed
+}
+
+protocol AnyPresenter {
+    var router : AnyRouter?{ get set}
+    var interactor : AnyInteractor? {get set}
+    var view : AnyView? {get set}
+    
+    func interactorDidDownloadCrypto(resault: Result<[Crypto],Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    var router: (any AnyRouter)?
+    
+    var interactor: (any AnyInteractor)? {
+        didSet{
+            interactor?.downloadCryptos()
+        }
+    }
+    
+    var view: (any AnyView)?
+    
+    func interactorDidDownloadCrypto(resault: Result<[Crypto], any Error>) {
+        switch resault {
+        case .success(let cryptos):
+                //view.update
+            view?.update(with: cryptos)
+        case .failure(_):
+                //view.update error
+            view?.update(with: "Try again later....")
+            
+            
+        }
+    }
+    
+    
+}
